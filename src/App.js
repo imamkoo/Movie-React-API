51f948d5fb78ac611baf501afad3b2c1214f6cbf
imamkoo <imamkholidubaidillah16@gmail.com>
2022-11-19 15:43:25 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from "react";
+
+const imgURL = process.env.REACT_APP_BASEIMGURL;
 
 function App() {
+  const [TopRatedMovies, setTopRatedMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setTopRatedMovies(result);
+    });
+  }, []);
+
+  const TopRatedMovieList = () => {
+    return TopRatedMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img
+            className="Movie-image"
+            alt=""
+            src={`${imgURL}/${movie.poster_path}`}
+            width="300px"
+          />
+          <div className="Movie-date">Release : {movie.release_date}</div>
+          <div className="Movie-rate">⭐ {movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
+  const Search = async (q) => {
+    if (q.length >= 3) {
+      const query = await searchMovie(q);
+      setTopRatedMovies(query.results);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie App</h1>
+        <input
+          className="Movie-search"
+          placeholder="Search..."
+          onChange={({ target }) => Search(target.value)}
+        />
+        <div className="Movie-container">
+          <TopRatedMovieList />
+        </div>
       </header>
     </div>
   );
