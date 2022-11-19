b1530d30edecf298c37adc12a149baf3e68773c7
imamkoo <imamkholidubaidillah16@gmail.com>
2022-11-19 17:45:20 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { getMovieList, searchMovie } from "./api";
 import { useEffect, useState } from "react";
-require("dotenv").config();
 
 const imgURL = process.env.REACT_APP_BASEIMGURL;
 
